2572bc19a3513d5f0991a1fcf4136f2da7f3370f
chayoung-choi <chayoung.dev@gmail.com>
2021-06-08 17:23:19 +0900

    2.0 Using Firebase Auth

--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -9,5 +10,5 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
     appId: process.env.REACT_APP_APP_ID,
 };
-
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
